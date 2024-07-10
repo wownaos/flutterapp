786e7357de7520987cd360e1ea8196735d40c71d
jaeyoung <wownaos@naver.com>
2024-07-10 21:39:02 +0900

    future 객체를 통한 screen 이동에서 beamer를 통해 페이지 이동으로 변경

--- radish_app/lib/main.dart
+++ radish_app/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
-import 'splash_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
+import 'router/locations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -42,7 +44,29 @@ class MyApp extends StatelessWidget {
   }
   StatelessWidget _splashLodingWidget(AsyncSnapshot<Object> snapshot) {
     if (snapshot.hasError) {print('에러가 발생했습니다.');  return Text('error');}
-    else if (snapshot.hasData) {return HomeScreen();}
+    else if (snapshot.hasData) {return RadishApp();}
     else {return SplashScreen();}
   }
 }
+
+class RadishApp extends StatelessWidget {
+  const RadishApp({Key? key}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // beamer에게 모든 페이지 이동권한을 줌
+    return MaterialApp.router(
+      // 하위 서브 클래스들이 init을 할 수 있도록 해주는 역할
+      routeInformationParser: BeamerParser(),
+      // routerDelegate : 'beamer instance name' parser가 해석한 정보를 전달받을 객체
+      routerDelegate: _routerDelegate,
+    );
+  }
+}
+
+final _routerDelegate = BeamerDelegate(
+    locationBuilder: BeamerLocationBuilder(
+      // beamLocations 는 Beamer에게 어떤 화면을 맡길지 알려주는 파라미터입니다.
+      beamLocations: [HomeLocation()]
+    )
+);
---
+++ radish_app/lib/utils/logger.dart
@@ -0,0 +1,4 @@
+import 'package:logger/logger.dart';
+
+final logger = Logger();
+
